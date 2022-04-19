409df62dd15037d97557515971026af840d0b4d0
Kaj <kb223aw@student.lnu.se>
2022-04-19 11:26:31 +0200

    setup next.js ts

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Elastic assignment</title>
+      </Head>
+      <h1>Elastic assignment</h1>
+    </div>
+  )
+}
+
+export default Home
