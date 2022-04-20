e51a10c5c545260f7db620af8819d2f7e2d0300c
Kaj <kb223aw@student.lnu.se>
2022-04-20 11:55:15 +0200

    add info from elastic

--- pages/api/elastic.ts
+++ pages/api/elastic.ts
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 import React from "react"
 import { Client } from "@elastic/elasticsearch"
 import type { NextApiRequest, NextApiResponse } from "next"
@@ -9,12 +7,6 @@ export async function connectToElasticsearch() {
   const FINGERPRINT = process.env.NEXT_PUBLIC_FINGERPRINT!
   const ELASTIC_USER_NAME = process.env.NEXT_PUBLIC_ELASTIC_USER_NAME!
   const ELASTIC_USER_PASSWORD = process.env.NEXT_PUBLIC_ELASTIC_USER_PASSWORD!
-  console.log(FINGERPRINT)
-  console.log(ELASTIC_USER_NAME)
-  console.log(ELASTIC_USER_PASSWORD)
-  if (!FINGERPRINT || !ELASTIC_USER_NAME || !ELASTIC_USER_PASSWORD) {
-    return "ERR_ENV_NOT_DEFINED"
-  }
 
   return new Client({
     node: "https://localhost:9200",
@@ -36,24 +28,46 @@ export default async function searchElastic(
   try {
     const client = await connectToElasticsearch()
     console.log("------------start client----------")
-    console.log(client)
+
+    if (
+      process.env.NEXT_PUBLIC_FINGERPRINT &&
+      process.env.NEXT_PUBLIC_ELASTIC_USER_NAME &&
+      process.env.NEXT_PUBLIC_ELASTIC_USER_PASSWORD
+    ) {
+      const exists = await client.exists({
+        index: "netflix_data",
+        id: "jXc2RoABv8pN6_CUBg86",
+      })
+      const response = await client.search({
+        index: "netflix_data",
+        size: 100,
+        query: {
+          match: {
+            title: "The",
+          },
+        },
+      })
+      console.log(response.hits.hits)
+      console.log(exists)
+    } else {
+      throw new Error("Failed to connect to Elasticsearch")
+    }
+
     console.log("------------end client----------")
 
     let results: any = []
 
-    // const response = await client.search({
-    //   index: "netflixdata",
-    //   size: 100,
-    //   query: {
-    //     match: {
-    //       title: "The",
-    //     },
-    //   },
-    // })
+    const response = await client.search({
+      index: "netflix_data",
+      size: 100,
+      query: {
+        match: {
+          title: "The",
+        },
+      },
+    })
 
-    console.log(client)
-
-    return res.send(client)
+    return res.send("kossa")
   } catch (error: any) {
     console.log(error)
     return res.status(error.statusCode || 500).json({ error: error.message })
