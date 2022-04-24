e1f2623186d90093408d2dfe061c5a408b7c3a42
Kaj <kb223aw@student.lnu.se>
2022-04-24 20:44:04 +0200

    add creating indices / bulk

--- pages/api/elastic.ts
+++ pages/api/elastic.ts
@@ -1,15 +1,111 @@
+//@ts-nocheck
+
+import client from "@elastic/elasticsearch/lib/client"
 import type { NextApiRequest, NextApiResponse } from "next"
 import { elasticSearchClient } from "../../components/elasticConnector"
-import { searchQuery, searchQuery2 } from "../../components/elasticQuery"
+import { searchQuery2 } from "../../components/elasticQuery"
+import fs from "fs-extra"
+
 /**
- * Function for getting the data from elastic search
+ * Reading CSV file and returning array of objects
  *
+ * @param {*} url
  * @return {*}
  */
+async function convertCSVtoArray(url) {
+  const data = await fs.readFile(url, "utf8")
+  const lines = data.split("\n")
+  const result = []
+  const headers = lines[0].split(",")
+  for (let i = 1; i < lines.length; i++) {
+    const obj = {}
+    const currentline = lines[i].split(",")
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j]
+    }
+    result.push(obj)
+  }
+  return result
+}
+
+async function bulkData(data) {
+  try {
+    // Creating movies index
+    await client.indices.create(
+      {
+        index: "movies",
+      },
+      { ignore: [400] }
+    )
+
+    // Create operations for bulk indexing
+    const operations = data.flatMap((doc) => [
+      { index: { _index: "movies" } },
+      doc,
+    ])
+
+    // Bulk indexing data for the elastic client
+    const bulkResponse = await client.bulk({ refresh: true, operations })
+    if (bulkResponse.errors) {
+      const erroredDocuments = []
+      // Itterate through errored documents and logging them
+      bulkResponse.items.forEach((action, i) => {
+        const operation = Object.keys(action)[0]
+        if (action[operation].error) {
+          erroredDocuments.push({
+            status: action[operation].status,
+            error: action[operation].error,
+            operation: body[i * 2],
+            document: body[i * 2 + 1],
+          })
+        }
+      })
+      console.log(erroredDocuments)
+    }
+  } catch (error) {}
+}
+
 async function searchElastic() {
   try {
     // Creating ES client
     const client = await elasticSearchClient()
+
+    // READ CSV FILE
+    const data = await convertCSVtoArray("./data/movies.csv")
+    console.log(data)
+
+    // Set indexes and id for all documents
+    await client.indices.create(
+      {
+        index: "movies",
+      },
+      { ignore: [400] }
+    )
+
+    // Create operations for bulk indexing
+    const operations = data.flatMap((doc) => [
+      { index: { _index: "movies" } },
+      doc,
+    ])
+
+    // Bulk indexing data for the elastic client
+    const bulkResponse = await client.bulk({ refresh: true, operations })
+    if (bulkResponse.errors) {
+      const erroredDocuments = []
+      // Itterate through errored documents and logging them
+      bulkResponse.items.forEach((action, i) => {
+        const operation = Object.keys(action)[0]
+        if (action[operation].error) {
+          erroredDocuments.push({
+            status: action[operation].status,
+            error: action[operation].error,
+            operation: body[i * 2],
+            document: body[i * 2 + 1],
+          })
+        }
+      })
+      console.log(erroredDocuments)
+    }
 
     // Creating search query
     const query: any = searchQuery2()
