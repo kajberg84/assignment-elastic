1d227de7c8720a2f300e7e7e096af31e82440dbf
Kaj <kb223aw@student.lnu.se>
2022-04-25 16:38:25 +0200

    add create data

---
+++ pages/api/createData.ts
@@ -0,0 +1,126 @@
+//@ts-nocheck
+
+import type { NextApiRequest, NextApiResponse } from "next"
+import { elasticSearchClient } from "../../components/elasticConnector"
+import { searchQuery2 } from "../../components/elasticQuery"
+import fs from "fs-extra"
+
+/**
+ * Reading CSV file and returning array of objects
+ *
+ * @param {*} url
+ * @return {*}
+ */
+async function convertCSVtoArray(url) {
+  const data = fs.readFileSync(url, { encoding: "utf8" })
+
+  const rows = data.split(/\r\n|\n/)
+  const headings = rows[0].split(",")
+  const newArray = []
+
+  for (let row = 1; row < rows.length; row++) {
+    const object = {}
+    let headingIndex = 0
+    let item = ""
+    // Changing csv for easier handling
+    for (let i = 0; i < rows[row].length; i++) {
+      if (rows[row].charAt(i) === ",") {
+        if (rows[row].charAt(i + 1) !== " ") {
+          object[headings[headingIndex]] = item
+          headingIndex++
+          item = ""
+        } else {
+          item += rows[row].charAt(i)
+        }
+      } else {
+        item += rows[row].charAt(i)
+      }
+    }
+    newArray.push(object)
+  }
+
+  newArray.map((item) => {
+    item.budget = parseInt(item.budget)
+  })
+
+  return newArray
+}
+
+async function createData() {
+  try {
+    // Creating ES client
+    const client = await elasticSearchClient()
+
+    await client.indices.delete({
+      index: "movies",
+    })
+
+    // READ CSV FILE
+    const data = await convertCSVtoArray("./data/movies.csv")
+
+    // Set indexes and id for all documents
+    await client.indices.create(
+      {
+        index: "movies",
+        operations: {
+          mappings: {
+            properties: {
+              id: { type: "integer" },
+              text: { type: "text" },
+              user: { type: "keyword" },
+              time: { type: "date" },
+            },
+          },
+        },
+      },
+      { ignore: [400] }
+    )
+
+    // Create operations for bulk indexing
+    const operations = data.flatMap((doc) => [
+      { index: { _index: "movies" } },
+      doc,
+    ])
+
+    // Bulk indexing data for the elastic client
+    const bulkResponse = await client.bulk({ refresh: true, operations })
+    if (bulkResponse.errors) {
+      console.log("Bulk indexing errors")
+      console.log(bulkResponse.errors)
+    } else {
+      console.log("Bulk indexing successful")
+    }
+
+    // console.log(bulkResponse.items)
+
+    const count = await client.count({ index: "movies" })
+    console.log(count)
+    // Creating search query
+    const query: any = searchQuery2()
+
+    // Searching with query
+    const response = await client.search(query)
+
+    const allBuckets: any = []
+    const aggre: any = response.aggregations
+
+    // om jag vill se hur datan ser ut
+    // console.log(aggre)
+
+    aggre.reldate_popu.buckets.forEach((item: any) => {
+      allBuckets.push(item)
+    })
+    // console.log(allBuckets)
+    return allBuckets
+  } catch (error: any) {
+    console.log(error.message)
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const aggregations = await createData()
+  res.status(200).json({ aggregations })
+}
